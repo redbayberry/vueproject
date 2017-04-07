c7c020f1634479dffa947f6bc3e48458850d0f83
redbayberry <476649860@qq.com>
2017-04-07 15:45:01 +0800

    vue webapp project

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="goods">
+  	<div class="menu-wrapper">
+     <ul>
+       <li v-for="item in goods">
+         <span class="text">{{item.name}}</span>
+       </li>
+     </ul> 
+    </div>
+  	<div class="foods-wrapper"></div>
+  </div>
+</template>
+<script>
+const ERR_OK = 0
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      goods: []
+    }
+  },
+  created() {
+    this.$http.get('/api/goods').then((response) => {
+      response = response.body
+      console.log(response)
+      if (response.errno === ERR_OK) {
+        this.goods = response.data
+      }
+    })
+  }
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+.goods
+  position: absolute
+  display: flex
+  top: 174px
+  bottom: 46px
+  width: 100%
+  overflow: hidden
+  .menu-wrapper
+    flex: 0 0 80px
+    width: 80px
+    background: #f3f5f7
+  .foods-wrapper
+  	flex: 1	
+</style>
---
+++ src/main.js
@@ -0,0 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import Seller from 'components/seller/seller'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/Ratings/Ratings'
+import App from './App'
+
+Vue.config.productionTip = false
+
+import 'common/stylus/index.styl'
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+// 路由表
+const routes = [
+  {path: '/seller', component: Seller},
+  {path: '/goods', component: Goods},
+  {path: '/ratings', component: Ratings}
+]
+
+const router = new VueRouter({
+  linkActiveClass: 'active',
+  routes
+})
+// 刚开始打开网页是默认激活一个链接
+router.push('/ratings')
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
+
