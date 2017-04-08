b40b4b0e6d108670e0b9277ca6cf7771e7cd9fe7
redbayberry <476649860@qq.com>
2017-04-08 18:30:35 +0800

    商品布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="goods">
-  	<div class="menu-wrapper">
+  	<div class="menu-wrapper" ref="menuWrapper">
      <ul>
-       <li v-for="item in goods">
-         <span class="text">{{item.name}}</span>
+       <li v-for="item in goods" class="menu-item">         
+         <span class="text"><span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}</span>
        </li>
      </ul> 
     </div>
-  	<div class="foods-wrapper"></div>
+  	<div class="foods-wrapper" ref="foodsWrapper">
+     <ul>
+       <li v-for="item in goods" class="food-list">
+         <h1 class="title">{{item.name}}</h1>
+         <ul>
+           <li v-for="food in item.foods" class="food-item border-1px">
+             <div class="icon">
+               <img :src="food.icon" width="57" height="57">
+             </div>
+             <div class="content">
+               <h2 class="name">{{food.name}}</h2>
+               <p class="desc">{{food.description}}</p>
+               <div class="extra">
+                 <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
+               </div>
+               <div class="price">
+                 <span class="now">￥{{food.price}}</span>
+                 <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+               </div>
+             </div>
+           </li>
+         </ul>
+       </li>
+     </ul> 
+    </div>
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 const ERR_OK = 0
 export default {
   props: {
@@ -23,18 +48,29 @@ export default {
       goods: []
     }
   },
-  created() {
+  mounted() {
+    this.classMap = ['decrease', 'discount', 'guarantee', 'special', 'invoice']
     this.$http.get('/api/goods').then((response) => {
       response = response.body
-      console.log(response)
       if (response.errno === ERR_OK) {
         this.goods = response.data
+        this.$nextTick(() => {
+          this._initScroll()
+        })
       }
     })
+  },
+  methods: {
+    _initScroll() {
+      console.log(this.$refs.menuWrapper)
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {})
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {})
+    }
   }
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixin"
 .goods
   position: absolute
   display: flex
@@ -46,6 +82,69 @@ export default {
     flex: 0 0 80px
     width: 80px
     background: #f3f5f7
+    .menu-item
+      display: table
+      height: 54px
+      width:56px
+      line-height: 14px
+      margin:0 auto
+      .icon
+        margin-right: 2px
+        icon(12px,12px,3)
+      .text
+        display: table-cell
+        font-size: 12px
+        vertical-align:middle
+        border-1px(rgba(7,17,27,0.2))        
   .foods-wrapper
-  	flex: 1	
+    flex: 1	
+    .title
+      height: 26px
+      line-height: 26px
+      border-left: 2px solid #d9dde1
+      padding-left: 14px
+      background: #f3f5f7
+      font-size: 12px
+      color: rgb(147,153,159)
+    .food-item
+      display: flex
+      margin: 18px
+      padding-bottom: 18px
+      &.border-1px
+        border-1px(rgba(7,17,27,0.1))
+      &:last-child
+        border-none()
+        margin-bottom: 0
+      .icon
+        flex: 0 0 57px
+        margin-right: 10px
+      .content
+        flex: 1
+        .name
+          margin: 2px 0 8px 0
+          height: 14px
+          line-height:14px
+          font-size: 14px
+          color: rgb(7,17,27)
+        .desc,.extra  
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147,153,159)
+        .desc
+          margin-bottom: 8px
+          line-height: 12px
+        .extra
+          .count
+            margin-right: 12px
+        .price
+          font-weight: 700
+          line-height: 24px
+          .now
+            margin-right: 8px
+            font-size: 14px
+            color: rgb(240,20,20)
+          .old
+            font-size: 10px
+            color: rgb(147,153,159)
+          
 </style>
