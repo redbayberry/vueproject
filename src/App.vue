be3dfcfc9cf411679aa55eab3c517dabb5c48010
redbayberry <476649860@qq.com>
2017-04-23 01:56:04 +0800

    给seller添加id属性

--- src/App.vue
+++ src/App.vue
@@ -12,24 +12,35 @@
         <router-link to='/seller'>商家</router-link>
       </div>
     </div>
-    <router-view :seller="seller"></router-view> 
+    <keep-alive>
+    <router-view :seller="seller"></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import {urlParse} from 'common/js/util'
 import Header from 'components/header/header'
 const ERR_OK = 0
 export default {
   data() {
     return {
-      seller: {}
+      seller: {
+        id: (() => {
+          let queryParam = urlParse()
+          console.log(queryParam)
+          return queryParam.id
+        })()
+      }
     }
   },
   created() {
-    this.$http.get('/api/seller').then((response) => {
+    this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
       response = response.body
       if (response.errno === ERR_OK) {
-        this.seller = response.data
+        // 直接这样赋值id会被丢弃
+        // this.seller = response.data
+        this.seller = Object.assign({}, this.seller, response.data)
       }
     })
   },
